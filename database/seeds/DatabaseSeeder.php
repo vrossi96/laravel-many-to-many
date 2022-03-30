d40d07a2f73616e12040ab3b3b0d2ce6e2492169
vrossi96 <vrossi396@yahoo.it>
2022-03-30 16:50:47 +0200

    seed for tag

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([PostSeeder::class, UserSeeder::class]);
+        $this->call([TagSeeder::class, PostSeeder::class, UserSeeder::class]);
     }
 }
